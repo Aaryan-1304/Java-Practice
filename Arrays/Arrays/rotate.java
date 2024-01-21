bdf5f8d193e2304572f2544e975076d9bb4838b6
Aaryan-1304 <aaryanbutala13@gmail.com>
2024-01-22 00:43:22 +0530

    2 jan 24

---
+++ Arrays/Arrays/rotate.java
@@ -0,0 +1,40 @@
+package Arrays.Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class rotate{
+
+    static int[] roatatearray(int arr[], int n){
+        int temp = arr[0];
+        for(int i=1; i<n; i++){
+            arr[i-1] = arr[i];
+        }
+        arr[n-1] = temp;
+        return arr;
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Size of the array is: ");
+        int size = in.nextInt();
+
+        int arr[] = new int[size];
+        System.out.println("Enter the elements of the array: ");
+        for(int i=0; i<size; i++){
+            arr[i] = in.nextInt();
+        }
+
+        System.out.println("Enter the number of rotations: ");
+        int rotations = in.nextInt();
+
+        System.out.println("Original Array: " + Arrays.toString(arr));
+
+        for(int i=0; i<rotations; i++){
+            arr = roatatearray(arr, size);
+        }
+        System.out.println("Array after " + rotations + " left rotations: " + Arrays.toString(arr));
+
+        in.close();
+    }
+}
